e277e71729f60a8e33d6eac94c7dd8249aed042a
Joshua Gage <josh@chezgage.com>
2018-02-03 11:55:23 -0800

    Stub of file to store secret API keys

---
+++ secrets.py
@@ -0,0 +1,11 @@
+# DO NOT check in this file.
+# Modify this file to use your secret API keys.
+
+# To ignore your changes, run:
+# git update-index --assume-unchanged secrets.py
+
+# ref: https://stackoverflow.com/questions/1139762/ignore-files-that-have-already-been-committed-to-a-git-repository/11366713#11366713
+
+# Get your secret API keys here: https://developers.themoviedb.org
+apiKey = "secretKey"
+accessToken = "secretToken"
